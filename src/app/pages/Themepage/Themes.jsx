a48d7b703af6ce396d221174b5f8d077a68f4efa
ulysses <ulyssesgutierrez0221@gmail.com>
2025-08-04 22:53:29 -0500

    commiting structure and timer component

---
+++ src/app/pages/Themepage/Themes.jsx
@@ -0,0 +1,20 @@
+import './Themes.css';
+
+function ThemePage(){
+
+return (
+    <body>
+        <div id="centerContainer">
+        <span>
+            Choose
+        </span>
+        <div id="firstButtons">
+            <button id="boyBtn"></button>
+            <button id="girlBtn"></button>
+        </div>
+        </div>
+    </body>
+    )
+}
+
+export default ThemePage;
